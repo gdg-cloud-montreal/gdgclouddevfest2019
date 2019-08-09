44ea8f258561624113e49800f3695d8a18adbb20
Ayrat Khayretdinov <akhayretdinov@cloudops.com>
2019-08-09 13:56:18 -0400

    devfest2019

---
+++ data/faq.md
@@ -0,0 +1,59 @@
+## General
+
+### Dates and location
+
+GDG Montreal DevFest will take place on the 28th of September 2019 at [Shopify]( https://goo.gl/Zwi76g) in Montreal.
+
+### Stay Informed
+
+To stay up to date on the latest information on sessions, speakers, and activities, be sure to visit the [DevFest website](https://gdgmontreal.com/), RSVP on [Meetup]( https://www.meetup.com/GDG-Montreal/events/254850545/) and follow us on [Facebook](https://www.facebook.com/AndroidMontreal) and [Twitter](https://twitter.com/@gdgmontreal). You can also follow and join the social conversation about GDG Montreal DevFest via official hashtags #Devfest19.
+
+### Content Formats
+
+During two conference days attendees will be able to attend sessions and hands-on workshops, chat with experts and attendees at coffee breaks, and more.
+
+  
+## Registration
+
+The window to buy a ticket will be opened until 6PM on September 23rd 2019, or earlier if the tickets are sold out. To buy a ticket [follow the link]( https://www.eventbrite.ca/e/devfest-2018-tickets-50486474404), select required ticket type, fill in the required information and pay for it.
+
+Once you've successfully payed for the ticket you’ll reach a confirmation screen and receive an email with the ticket. Please don’t forget to download it on your device or print it, as it’s required for entrance.
+
+### Ticket Types
+
+#### **General Admission tickets**
+
+General Admission: $20
+
+Conditions: Valid for the full day
+
+#### **Student tickets**
+
+Student Admission: $10
+
+Conditions: A valid student card must be shown upon entrance
+
+### Registration Terms & Conditions
+
+- Tickets for GDG Montreal DevFest are for the full two-day conference and single-day tickets are available. 
+- You may buy a ticket on behalf of the other person (for example, make a gift). Just mention during purchase the name, company and title of the person who will attend the event. 
+- GDG Montreal DevFest tickets must be used by the original registrant and are non-transferable.  
+- At check-in, we’ll check your ticket prior to assigning you your GDG Montreal DevFest badge. If you don’t have proper identification, you won’t be able to receive a badge and won’t be admitted to the conference. 
+- If you buy an EDU ticket and can't provide academic documentation during check-in at GDG Montreal DevFest, you won't be admitted or offered a refund, nor will you be able to convert your ticket to general admission. Please be sure to bring a current student ID or other proof that you are qualified to receive this special rate when you check in at the conference. 
+- Attendees aren’t permitted to bring guests to GDG Montreal DevFest or to the after hours party. If you have someone traveling with you, they’ll need to register themselves and purchase an attendee ticket. 
+- Photographs and/or video taken at GDG Montreal DevFest by GDG Montreal, or others on behalf of the conference organizers, may include your image or likeness. You agree that GDG Montreal may use such photographs and/or video for any purpose without any compensation to you. 
+- All information entered into the registration form must be correct and accurate to the best of your knowledge. All information expected to be entered in English. Some of the information entered (such as name, company and title), will be displayed on the attendee badge.  
+- All registered attendees agree to allow GDG Montreal to contact them regarding their registration and attendance at the event. By registering for a ticket, you agree to allow GDG Montreal to communicate with you via email with information regarding the event. 
+- You agree to be solely responsible for your own safety, belongings and well-being while participating in GDG Montreal DevFest. Organizers won't be liable for your participation in GDG Montreal DevFest. 
+- Applicants and attendees can request the deletion of their registration data after the event ends by emailing [info@android-montreal.com](mailto:info@android-montreal.com). 
+  
+
+## Attendance Details
+
+### Language
+
+All presentations at GDG Montreal DevFest will be in English.
+
+### Event Attire
+
+GDG DevFest is a developer event, so please be comfortable and casual. There is no enforced dress code.
---
+++ internals/import-default-data.js
@@ -0,0 +1,192 @@
+import { initializeFirebase, firestore } from './firebase-config';
+import data from '../docs/default-firebase-data.json';
+
+const importSpeakers = () => {
+  const speakers = data.speakers;
+  console.log('\tImporting', Object.keys(speakers).length, 'speakers...');
+
+  const batch = firestore.batch();
+
+  Object.keys(speakers).forEach((speakerId, order) => {
+    if (speakerId) {
+      batch.set(
+        firestore.collection('speakers').doc(speakerId),
+        {
+          ...speakers[speakerId],
+          order,
+        },
+      );
+    }
+  });
+
+  return batch.commit()
+    .then((results) => {
+      console.log('\tImported data for', results.length, 'speakers');
+      return results;
+    });
+};
+
+const importTeam = () => {
+  const teams = data.team;
+  console.log('\tImporting', Object.keys(teams).length, 'subteam...');
+
+  const batch = firestore.batch();
+
+  Object.keys(teams).forEach((teamId) => {
+    if (teamId) {
+      batch.set(
+        firestore.collection('team').doc(teamId),
+        { title: teams[teamId].title },
+      );
+
+      teams[teamId].members.forEach((member, id) => {
+        batch.set(
+          firestore.collection('team').doc(`${teamId}`).collection('members').doc(`${id}`),
+          member,
+        );
+      });
+    }
+  });
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for', results.length, 'documents');
+      return results;
+    });
+};
+
+const importPartners = () => {
+  const partners = data.partners;
+  console.log('\tImporting partners...');
+
+  const batch = firestore.batch();
+
+  Object.keys(partners).forEach((docId) => {
+    if (docId) {
+      batch.set(
+        firestore.collection('partners').doc(docId),
+        { title: partners[docId].title,
+          order: partners[docId].order },
+      );
+
+      partners[docId].logos.forEach((item, id) => {
+        batch.set(
+          firestore.collection('partners').doc(`${docId}`).collection('items').doc(`${id}`.padStart(3, 0)),
+          item,
+        );
+      });
+    }
+  });
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for', results.length, 'documents');
+      return results;
+    });
+};
+
+const importGallery = () => {
+  const gallery = data.gallery;
+  console.log('\tImporting gallery...');
+
+  const batch = firestore.batch();
+
+  Object.keys(gallery).forEach((docId) => {
+    batch.set(
+      firestore.collection('gallery').doc(`${docId}`.padStart(3, 0)),
+      {
+        url: gallery[docId],
+        order: docId,
+      },
+    );
+  });
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for', results.length, 'images');
+      return results;
+    });
+};
+
+const importSessions = () => {
+  const docs = data.sessions;
+  console.log('\tImporting sessions...');
+
+  const batch = firestore.batch();
+
+  Object.keys(docs).forEach((docId) => {
+    if (docId) {
+      batch.set(
+        firestore.collection('sessions').doc(docId),
+        docs[docId],
+      );
+    }
+  });
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for', results.length, 'sessions');
+      return results;
+    });
+};
+
+const importSchedule = () => {
+  const docs = data.schedule;
+  console.log('\tImporting schedule...');
+
+  const batch = firestore.batch();
+
+  Object.keys(docs).forEach((docId) => {
+    if (docId) {
+      batch.set(
+        firestore.collection('schedule').doc(docId),
+        {
+          ...docs[docId],
+          date: docId,
+        },
+      );
+    }
+  });
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for', Object.keys(docs).length, 'days');
+      return results;
+    });
+};
+
+const importNotificationsConfig = async () => {
+  const notificationsConfig = data.notifications.config;
+  console.log('Migrating notifications config...');
+  const batch = firestore.batch();
+
+  batch.set(
+    firestore.collection('config').doc('notifications'),
+    notificationsConfig,
+  );
+
+  return batch.commit()
+    .then(results => {
+      console.log('\tImported data for notifications config');
+      return results;
+    });
+
+};
+
+initializeFirebase()
+  // .then(() => importGallery())
+  .then(() => importNotificationsConfig())
+  .then(() => importPartners())
+  .then(() => importSchedule())
+  .then(() => importSessions())
+  .then(() => importSpeakers())
+  .then(() => importTeam())
+
+  .then(() => {
+    console.log('Finished');
+    process.exit();
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit();
+  });
